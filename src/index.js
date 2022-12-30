d473fb42f84b3dcd5e284651772cf559de3616cf
Herson Guerrero Huh <watsrunner+github+herson@gmail.com>
2022-12-30 15:48:03 -0500

    First Scene

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import './style.css'
 import ReactDOM from 'react-dom/client'
+import { Canvas } from '@react-three/fiber'
+import Experience from './Experience.js'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
-    <>
-        Soon to be a badass R3F application
-    </>
+        <Canvas>
+            <Experience />
+        </Canvas>
 )
