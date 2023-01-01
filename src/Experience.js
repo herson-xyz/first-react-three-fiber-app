e6f92ae8cc4f82a4328ec7cf41e424796553a34c
Herson Guerrero Huh <watsrunner+github+herson@gmail.com>
2023-01-01 16:58:13 -0500

    Grouping elements and performing actions on the group, such as animating the sphere and the cube to rotate in a carousel fashion

--- src/Experience.js
+++ src/Experience.js
@@ -3,23 +3,27 @@ import { useRef } from "react"
 
 export default function Experience()
 {
-    const cubeRef = useRef() // This "attaches" a reference to an element in the DOM. Just like the example for updating the color in first-react-app
+    const cubeRef = useRef()  // This "attaches" a reference to an element in the DOM. Just like the example for updating the color in first-react-app
+    const groupRef = useRef() // Make a reference to the group
 
     useFrame((state, delta) =>
     {
         cubeRef.current.rotation.y += delta // We do this because different setups run at different framerates. Delta tells us how much time has passed since the last frame. So using delta here normalizes the speed for everyone.
+        groupRef.current.rotation.y += delta
     })
     
     return <>
-        <mesh position-x={ -2 }>
-            <sphereGeometry />
-            <meshBasicMaterial color="orange" />
-        </mesh>
+        <group ref={ groupRef }> {/* associate the reference to the group element */}
+            <mesh position-x={ -2 }>
+                <sphereGeometry />
+                <meshBasicMaterial color="orange" />
+            </mesh>
 
-        <mesh ref={cubeRef} rotation-y={ Math.PI * 0.25 } position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshBasicMaterial color="mediumpurple" />
-        </mesh>
+            <mesh ref={cubeRef} rotation-y={ Math.PI * 0.25 } position-x={ 2 } scale={ 1.5 }>
+                <boxGeometry />
+                <meshBasicMaterial color="mediumpurple" />
+            </mesh>
+        </group>
 
         <mesh position-y={ -1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
             <planeGeometry />
