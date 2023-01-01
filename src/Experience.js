c1664966237fd38a12066a4ac3ebd2efc2f85bac
Herson Guerrero Huh <watsrunner+github+herson@gmail.com>
2023-01-01 17:23:34 -0500

    Adding OrbitControls using 'extend' and useThree

--- src/Experience.js
+++ src/Experience.js
@@ -1,18 +1,26 @@
-import { useFrame } from "@react-three/fiber"
+import { useThree, extend, useFrame } from "@react-three/fiber"
 import { useRef } from "react"
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
+// extend({ OrbitControls: OrbitControls }) // On the left, the name of the declarative version of the OrbitControls class.
+extend({ OrbitControls })
 
 export default function Experience()
 {
+    const { camera, gl } = useThree()  // This gets the state at the beginning. Useful for referencing camera, scene, clock, etc
     const cubeRef = useRef()  // This "attaches" a reference to an element in the DOM. Just like the example for updating the color in first-react-app
     const groupRef = useRef() // Make a reference to the group
 
     useFrame((state, delta) =>
     {
         cubeRef.current.rotation.y += delta // We do this because different setups run at different framerates. Delta tells us how much time has passed since the last frame. So using delta here normalizes the speed for everyone.
-        groupRef.current.rotation.y += delta
+        // groupRef.current.rotation.y += delta
     })
     
     return <>
+        
+        <orbitControls args={ [ camera, gl.domElement ] } /> { /* Note 1 */ }
+
         <group ref={ groupRef }> {/* associate the reference to the group element */}
             <mesh position-x={ -2 }>
                 <sphereGeometry />
