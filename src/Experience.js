815b2c8231d5173abecaf53277865724ed01fe2e
Herson Guerrero Huh <watsrunner+github+herson@gmail.com>
2023-01-01 17:55:05 -0500

    Adding light

--- src/Experience.js
+++ src/Experience.js
@@ -2,40 +2,42 @@ import { useThree, extend, useFrame } from "@react-three/fiber"
 import { useRef } from "react"
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
-// extend({ OrbitControls: OrbitControls }) // On the left, the name of the declarative version of the OrbitControls class.
 extend({ OrbitControls })
 
 export default function Experience()
 {
-    const { camera, gl } = useThree()  // This gets the state at the beginning. Useful for referencing camera, scene, clock, etc
-    const cubeRef = useRef()  // This "attaches" a reference to an element in the DOM. Just like the example for updating the color in first-react-app
-    const groupRef = useRef() // Make a reference to the group
+    const { camera, gl } = useThree()
+    const cubeRef = useRef()  
+    const groupRef = useRef() 
 
     useFrame((state, delta) =>
     {
-        cubeRef.current.rotation.y += delta // We do this because different setups run at different framerates. Delta tells us how much time has passed since the last frame. So using delta here normalizes the speed for everyone.
+        cubeRef.current.rotation.y += delta 
         // groupRef.current.rotation.y += delta
     })
     
     return <>
         
-        <orbitControls args={ [ camera, gl.domElement ] } /> { /* Note 1 */ }
+        <orbitControls args={[camera, gl.domElement]} />
+        
+        <directionalLight position={[1, 2, 3]} intensity={1.5} />
+        <ambientLight intensity={0.5} /> {/* We add ambient light so that the darker shadows aren't unrealistically dark*/}
 
-        <group ref={ groupRef }> {/* associate the reference to the group element */}
+        <group ref={ groupRef }>
             <mesh position-x={ -2 }>
                 <sphereGeometry />
-                <meshBasicMaterial color="orange" />
+                <meshStandardMaterial color="orange" />
             </mesh>
 
             <mesh ref={cubeRef} rotation-y={ Math.PI * 0.25 } position-x={ 2 } scale={ 1.5 }>
                 <boxGeometry />
-                <meshBasicMaterial color="mediumpurple" />
+                <meshStandardMaterial color="mediumpurple" />
             </mesh>
         </group>
 
         <mesh position-y={ -1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
             <planeGeometry />
-            <meshBasicMaterial color="greenyellow" />
+            <meshStandardMaterial color="greenyellow" />
         </mesh>
     </>
 }
