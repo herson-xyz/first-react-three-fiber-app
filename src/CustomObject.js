f09517717b4cdbbd0de6ffb0b960d8fdc02d5ca0
Herson Guerrero Huh <watsrunner+github+herson@gmail.com>
2023-01-02 14:03:33 -0500

    useMemo to optimize the calculation of vertices

--- src/CustomObject.js
+++ src/CustomObject.js
@@ -1,17 +1,28 @@
+import { useMemo } from 'react'
 import * as THREE from 'three' // We did this so we could use THREE.Doubleside
 
 export default function CustomObject()
 {
-    const verticesCount = 10 * 3 // 10 triangles with 3 vertices each
-    const positions = new Float32Array(verticesCount * 3) // Each vertex needs three values: x, y, z
+    const verticesCount = 10 * 3 
 
-    for (let i = 0; i < verticesCount * 3; i++)
-        positions[i] = (Math.random() - 0.5) * 3
+    const positions = useMemo(() =>
+    {
+        // We include the calculation of the object's position here
+        // in the case that the CustomObject needs to be re-rendered.
+        // useMemo helps us save the vertex information in "cache"
+        const positions = new Float32Array(verticesCount * 3) 
 
+        for (let i = 0; i < verticesCount * 3; i++)
+            positions[i] = (Math.random() - 0.5) * 3
+        
+        return positions
+
+    }, [])
+    
     return <mesh>
         <bufferGeometry>
             <bufferAttribute 
-                attach="attributes-position" // This is an abstraction
+                attach="attributes-position"
                 count={verticesCount}
                 itemSize={3}
                 array={positions}
