148812041ca0210be2987ffd59b05c6ff4d2c75d
Herson Guerrero Huh <watsrunner+github+herson@gmail.com>
2023-01-03 06:54:23 -0500

    Final changes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import Experience from './Experience.js'
+import * as THREE from 'three'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
@@ -15,6 +16,14 @@ const cameraSettings =
 
 root.render(
     <Canvas
+        // These are the default settings in R3F. Adding them here to make
+        // them more visible, but these can be removed, unless changed.
+        dpr= { [ 1, 2] }
+        gl={{
+            antialias: true,
+            toneMapping: THREE.ACESFilmicToneMapping,
+            outputEncoding: THREE.sRGBEncoding
+        }}
         camera={ cameraSettings }
     >
         <Experience />
