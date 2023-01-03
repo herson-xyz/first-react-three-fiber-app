eecc1519f4bc84f50fb737514b7d6977445d45f5
Herson Guerrero Huh <watsrunner+github+herson@gmail.com>
2023-01-03 10:09:07 -0500

    Transform Controls

--- src/Experience.js
+++ src/Experience.js
@@ -1,11 +1,15 @@
-import { OrbitControls } from "@react-three/drei"
+import { TransformControls, OrbitControls } from "@react-three/drei"
+import { useRef } from 'react'
 
 export default function Experience()
 {
-    
+    const cube = useRef()
+
     return <>
         
-        <OrbitControls />
+        {/* We makeDefault to tell R3F that these are our default controls */}
+        {/* Otherwise, the camera moves when moving the Transform Controls */}
+        <OrbitControls makeDefault /> 
                 
         <directionalLight position={[1, 2, 3]} intensity={1.5} />
         <ambientLight intensity={0.5} /> {/* We add ambient light so that the darker shadows aren't unrealistically dark*/}
@@ -15,10 +19,21 @@ export default function Experience()
             <meshStandardMaterial color="orange" />
         </mesh>
 
-        <mesh rotation-y={ Math.PI * 0.25 } position-x={ 2 } scale={ 1.5 }>
+        {/* Two Ways of Implementing Transform Controls */}
+
+        {/* <TransformControls position-x={2}>
+            <mesh rotation-y={Math.PI * 0.25} scale={1.5}>
+                <boxGeometry />
+                <meshStandardMaterial color="mediumpurple" />
+            </mesh>
+        </TransformControls> */}
+            
+        <mesh ref={cube} rotation-y={ Math.PI * 0.25 } position-x={ 2 } scale={ 1.5 }>
             <boxGeometry />
             <meshStandardMaterial color="mediumpurple" />
         </mesh>
+
+        <TransformControls object={cube} mode="translate" />  
 
         <mesh position-y={ -1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
             <planeGeometry />
