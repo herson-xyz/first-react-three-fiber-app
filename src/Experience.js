4f5d8edb57dbc8f096448a7530e943e92a0ca2ee
Herson Guerrero Huh <watsrunner+github+herson@gmail.com>
2023-01-04 21:42:22 -0500

    Enviroment Map - Using Stage Helper

--- src/Experience.js
+++ src/Experience.js
@@ -1,8 +1,7 @@
 import { useFrame } from "@react-three/fiber"
 import { useRef } from "react"
-import { Lightformer, Environment, ContactShadows, OrbitControls } from '@react-three/drei'
+import { Stage, OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
-import { useControls } from 'leva'
 
 export default function Experience()
 {
@@ -12,82 +11,48 @@ export default function Experience()
     {
         cube.current.rotation.y += delta 
     })
-
-    const { color, opacity, blur } = useControls('contact shadows',
-    {
-        color: '#4b2709',
-        opacity: { value: 0.4, min: 0, max: 1 },
-        blur: { value: 2.8, min: 0, max: 10}
-        })
-    
-    const { envMapIntensity, envMapHeight, envMapRadius, envMapScale } = useControls('environment map',
-    {
-        envMapIntensity: { value: 3.5, min: 0, max: 12 },
-        envMapHeight: { value: 7, min: 0, max: 100 },
-        envMapRadius: { value: 20, min: 10, max: 1000 },
-        envMapScale: { value: 100, min: 10, max: 1000}
-    })
     
     return <>
-        
-
-        
-        <Environment
-            preset="sunset"
-            ground=
-            {{
-                height: envMapHeight,
-                radius: envMapRadius,
-                scale: envMapScale
-            }}>
-        </Environment>
-        
-        <color
-            args={['purple']}
-            attach="background" />
-        
+          
         <Perf
             position="top-left" />
         
         <OrbitControls
             makeDefault />
-        
-        <ContactShadows 
-            position={[0, 0, 0]}
-            scale={10}
-            resolution={512}
-            far={5}
-            color={color}
-            opacity={opacity}
-            blur={blur}
-            // frames={1} // Use this if the scene is complex and we need to bake
-            />
+        <Stage
+            contactShadow=
+            {{
+                opacity: 0.2,
+                blur: 3
+            }}
+            environment="sunset"
+            preset="portrait"
+            intensity={2}>
 
-        <mesh
-            castShadow
-            position-x={-2}
-            position-y={1}>
+            <mesh
+                castShadow
+                position-x={-2}
+                position-y={1}>
 
-            <sphereGeometry />
+                <sphereGeometry />
 
-            <meshStandardMaterial
-                color="orange"
-                envMapIntensity={ envMapIntensity }/>
-        </mesh>
+                <meshStandardMaterial
+                    color="orange"/>
+            </mesh>
 
-        <mesh
-            ref={cube}
-            castShadow
-            rotation-y={Math.PI * 0.25}
-            position-x={2}
-            position-y={1}
-            scale={1.5}>
+            <mesh
+                ref={cube}
+                castShadow
+                rotation-y={Math.PI * 0.25}
+                position-x={2}
+                position-y={1}
+                scale={1.5}>
 
-            <boxGeometry />
+                <boxGeometry />
 
-            <meshStandardMaterial
-                color="mediumpurple"
-                envMapIntensity={ envMapIntensity }/>
-        </mesh>
+                <meshStandardMaterial
+                    color="mediumpurple"/>
+                </mesh>
+        </Stage>
     </>
 }
