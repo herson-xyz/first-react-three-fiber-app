2646759be154d9cc0386829d9ff08fcf5e8a606a
Herson Guerrero Huh <watsrunner+github+herson@gmail.com>
2023-01-06 11:41:04 -0500

    Loading Models - Using GLTF -> React Three Fiber to create a react component from a GLTF and be able to manipulate a component's nodes programatically

--- src/Experience.js
+++ src/Experience.js
@@ -3,6 +3,7 @@ import { Perf } from 'r3f-perf'
 import { Suspense } from 'react'
 import Model from './Model.js'
 import Placeholder from './Placeholder.js'
+import Hamburguer from './Hamburger.js'
 
 export default function Experience()
 {
@@ -12,7 +13,12 @@ export default function Experience()
 
         <OrbitControls makeDefault />
 
-        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
+        <directionalLight
+            castShadow
+            position={[1, 2, 3]}
+            intensity={1.5}
+            //We add shadow bias to fix shadow acne
+            shadow-normalBias={0.04} />
         <ambientLight intensity={ 0.5 } />
 
         <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
@@ -22,7 +28,7 @@ export default function Experience()
 
         <Suspense
             fallback={ <Placeholder position-y={0.5} scale={[2, 3, 2]} /> }>
-            <Model />
+            <Hamburguer scale={0.35} />
         </Suspense>
     </>
 }
