1db688120ab32e8c7cbc5052835b4a2bffbbece1
Herson Guerrero Huh <watsrunner+github+herson@gmail.com>
2023-01-17 05:34:21 -0500

    3D Text - Basic implementation of Text3D + Center Drei Components

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## First React-Three-Fiber App
 
 ### GIF of Latest Progress
-![](./gifs/2023-1-6-1.gif)
+![](./gifs/2023-1-17-1.gif)
 
 ### Getting Started
 These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
--- src/Experience.js
+++ src/Experience.js
@@ -1,9 +1,5 @@
-import { OrbitControls } from '@react-three/drei'
+import { Center, Text3D, OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
-import { Suspense } from 'react'
-import Model from './Model.js'
-import Placeholder from './Placeholder.js'
-import Hamburguer from './Hamburger.js'
 
 export default function Experience()
 {
@@ -13,22 +9,20 @@ export default function Experience()
 
         <OrbitControls makeDefault />
 
-        <directionalLight
-            castShadow
-            position={[1, 2, 3]}
-            intensity={1.5}
-            //We add shadow bias to fix shadow acne
-            shadow-normalBias={0.04} />
-        <ambientLight intensity={ 0.5 } />
-
-        <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
-            <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
-
-        <Suspense
-            fallback={ <Placeholder position-y={0.5} scale={[2, 3, 2]} /> }>
-            <Hamburguer scale={0.35} />
-        </Suspense>
+        <Center>
+            <Text3D 
+            font="./fonts/helvetiker_regular.typeface.json"
+            size={0.75}
+            height={0.2}
+            curveSegments={12}
+            bevelEnabled
+            bevelThickness={0.02}
+            bevelSize={0.02}
+            bevelOffset={0}
+            bevelSegments={5}>
+                HELLO R3F
+                <meshNormalMaterial />
+            </Text3D>
+        </Center>
     </>
 }
