e96da1dcbda2db67b58bbd1b49fcb6e6ee7819d9
Herson Guerrero Huh <watsrunner+github+herson@gmail.com>
2023-01-17 06:48:06 -0500

    3D Text - Optimization Solution 2 - Using native Three.js outside of the Experience function to create the Geometry and Material that we then reuse using useEffect

--- src/Experience.js
+++ src/Experience.js
@@ -1,22 +1,35 @@
 import { useMatcapTexture, Center, Text3D, OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
-import { useState } from 'react'
+import * as THREE from 'three'
+import { useEffect } from 'react'
+
+const torusGeometry = new THREE.TorusGeometry(1, 0.6, 16, 32)
+const material = new THREE.MeshMatcapMaterial()
 
 export default function Experience()
 {   
-    const [ torusGeometry, setTorusGeometry ] = useState()
-    const [ material, setMaterial] = useState()
 
     const [matcapTexture] = useMatcapTexture('7B5254_E9DCC7_B19986_C8AC91', 256) 
+
+    useEffect(() =>
+    { 
+        matcapTexture.encoding = THREE.sRGBEncoding
+        material.needsUpdate = true
+        // When using materials created in native THREE, we lose the auto encoding that happens
+        // when we use materials created in react-three. So we set the encoding to sRGB with the
+        // to avoid the material being too bright
+        material.matcap = matcapTexture
+        material.needsUpdate = true
+        // When the material is created, it is being created without the matcap texture
+        // When you make matcap changes, you need to tell Three.js.
+        // Three.js will update the actual shader, and Three.js tries to do this as least as possible
+    }, [])
 
     return <>
 
         <Perf position="top-left" />
 
         <OrbitControls makeDefault />
-
-        <torusGeometry ref={ setTorusGeometry } args={[1, 0.6, 16, 32]} />
-        <meshMatcapMaterial ref={ setMaterial } matcap={ matcapTexture } />
 
         <Center>
             <Text3D
